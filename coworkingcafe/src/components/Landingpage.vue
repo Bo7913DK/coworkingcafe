2cf6882b7afd94a637a11988fa92ffc0bf8a8f13
Bo7913DK <bohkuntz@gmail.com>
2024-12-11 22:25:43 +0100

    Fikset hero-banner og lavet grøn banner-ting nedenunder

--- coworkingcafe/src/components/Landingpage.vue
+++ coworkingcafe/src/components/Landingpage.vue
@@ -1,15 +1,30 @@
 <script setup>
-import Navbar from './Navbar.vue';
 </script>
 
 <template>
   <div class="full-page">
-    <div class="hero-banner">
-      <img src="@/img/herobanner.png" alt="Hero Banner" class="hero-image">
+    <section class="hero-banner">
+      <img src="@/img/herobanner.png" alt="Hero Image" class="hero-image">
       <div class="hero-content">
         <img src="@/img/CwcHvidLogoTekst.svg" alt="Coworking Cafe Logo" class="hero-logo">
         <button class="booknu-button">BOOK NU</button>
       </div>
-    </div>
+    </section>
+    
+    <section class="info-box">
+      <div class="info-item">
+        <img src="@/img/PrisIkon.png" alt="Pris Ikon" class="info-icon">
+        <div class="info-text">Priser fra 4500 kr</div>
+      </div>
+      <div class="info-item">
+        <img src="@/img/LokationIkon.png" alt="Lokations Ikon" class="info-icon">
+        <div class="info-text">Central beliggenhed, uden naboer</div>
+      </div>
+      <div class="info-item">
+        <img src="@/img/GaesterIkon.png" alt="Antal Gæster Ikon" class="info-icon">
+        <div class="info-text">80 Siddepladser</div>
+      </div>
+    </section>
   </div>
+
 </template>
